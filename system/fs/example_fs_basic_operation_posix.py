f44d9f543701f12109273f2635239fb5451b9c52
rivern.yuan <1520483071@qq.com>
2023-11-13 15:32:12 +0800

    更新 LICENSE

--- system/fs/example_fs_basic_operation_posix.py
+++ system/fs/example_fs_basic_operation_posix.py
@@ -1,3 +1,17 @@
+# Copyright (c) Quectel Wireless Solution, Co., Ltd.All Rights Reserved.
+#  
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  
+#     http://www.apache.org/licenses/LICENSE-2.0
+#  
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # 创建一个test.txt文件,注意路径在/usr下
 f = open("/usr/test.txt", "w+")
 i = f.write("hello world\n")
