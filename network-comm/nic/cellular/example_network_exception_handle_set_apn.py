b42739ddc8f1d0e3c4ac2d3f1fccb634136ba5b4
Junchen <32856522+Mr-FJC@users.noreply.github.com>
2024-03-20 09:58:46 +0800

    增加拨号应用文档异常处理章节中设置apn的示例代码 (#10)
    * code (cellular): 提交蜂窝无线网卡应用文档中的示例代码
    
    固件版本: N/A
    
    是否需要文案翻译: 是
    
    * 增加拨号应用文档异常处理章节中apn配置例程
    
    * 根据最新要求将示例代码中的注释都换成英文
    
    ---------
    
    Co-authored-by: jayceon.fu <jayceon.fu@quectel.com>

--- network-comm/nic/cellular/example_network_exception_handle_set_apn.py
+++ network-comm/nic/cellular/example_network_exception_handle_set_apn.py
@@ -1,11 +1,11 @@
 # Copyright (c) Quectel Wireless Solution, Co., Ltd.All Rights Reserved.
-#  
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#  
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#  
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ from misc import Power
 
 # Configure the APN information according to your actual needs
 usrCfg = {'apn': '3gnet', 'username': '', 'password': ''}
-
 
 def checkAPN():
     # Get the APN information of the first cellular NIC and check if the current one is the one you specified
@@ -52,28 +51,7 @@ def main():
     stage, state = checkNet.waitNetworkReady(20)
     if stage == 3 and state == 1:
         print('Network connected successfully.')
-        # Create a socket object
-        sock = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
-        # Resolve the domain name
-        try:
-            sockaddr = usocket.getaddrinfo('python.quectel.com', 80)[0][-1]
-        except Exception:
-            print('Domain name resolution failed.')
-            sock.close()
-            return
-        # Connect to the server
-        sock.connect(sockaddr)
-        # Send data to the server
-        ret = sock.send('GET /News HTTP/1.1\r\nHost: python.quectel.com\r\nAccept-Encoding: deflate\r\nConnection: keep-alive\r\n\r\n')
-        print('send {} bytes'.format(ret))
-
-        # Receive data from the server
-        data = sock.recv(256)
-        print('recv {} bytes:'.format(len(data)))
-        print(data.decode())
-
-        # Close the connection
-        sock.close()
+        # do something
     else:
         print('Network connected failed, stage={}, state={}'.format(stage, state))
 
--- network-comm/nic/cellular/example_socket_manually_activate_one_nic_set_apn.py
+++ network-comm/nic/cellular/example_socket_manually_activate_one_nic_set_apn.py
@@ -18,31 +18,31 @@ import dataCall
 import utime
 from misc import Power
 
-# 用户需要配置的APN信息，根据实际情况修改
+# Configure the APN information according to your actual needs
 usrCfg1 = {'profileID': 1, 'apn': '3gnet', 'username': '', 'password': ''}
 
 '''
-环境配置：
-1.关闭开机自动拨号功能
-2.APN配置
+Environment configuration:
+1. Disable the feature of automatic data call at startup
+2. Configure APN
 '''
 def cfgEnv(usrCfg):
     errcode = 0
     need_reboot1 = False
     need_reboot2 = False
     if "datacall_config.json" not in uos.listdir('/usr'):
-        # 关闭第一路网卡开机自动激活功能
+        # Disable automatic activation at startup for the first cellular NIC
         dataCall.setAutoActivate(usrCfg['profileID'], 0)
-        # 建议用户使能网卡自动重连功能
+        # It is recommended to enable the automatic reconnection for the cellular NIC
         dataCall.setAutoConnect(usrCfg['profileID'], 1)
         need_reboot1 = True
 
     print('Check the APN configuration.')
-    # 获取网卡的APN信息，确认当前使用的是否是用户指定的APN
+    # Get the APN information of the cellular NICs and check if the current one is the one you specified
     pdpCtx = dataCall.getPDPContext(usrCfg['profileID'])
     if pdpCtx != -1:
         if pdpCtx[1] != usrCfg['apn']:
-            # 如果不是用户需要的APN，使用如下方式配置
+            # If it is not the APN you need, configure it as follows
             ret = dataCall.setPDPContext(usrCfg['profileID'], 0, usrCfg['apn'], usrCfg['username'], usrCfg['password'], 0)
             if ret == 0:
                 print('APN configuration successful.')
@@ -56,7 +56,7 @@ def cfgEnv(usrCfg):
     else:
         print('Failed to get PDP Context.')
         errcode = -1
-    # 重启使配置生效
+    # Reboot the module to make the configuration take effect
     if need_reboot1 or need_reboot2:
         print('Ready to restart.')
         print('Please re-execute this program after restarting.')
@@ -69,7 +69,7 @@ def main():
     if cfgEnv(usrCfg1) == -1:
         return
 
-    # 手动激活蜂窝无线网卡
+    # Manually activate the cellular NIC
     print('Prepare to activate the NIC{}.'.format(usrCfg1['profileID']))
     ret = dataCall.activate(usrCfg1['profileID'])
     if ret == -1:
@@ -79,33 +79,33 @@ def main():
     stage, state = checkNet.waitNetworkReady(10)
     if stage == 3 and state == 1:
         print('Network connected successfully.')
-        # 获取第一路网卡的IP地址等信息
+        # Get the IP address and other information of the first NIC
         ret = dataCall.getInfo(usrCfg1['profileID'], 0)
         print('NIC{}：{}'.format(usrCfg1['profileID'], ret))
 
         print('---------------sock test-----------------')
-        # 创建socket对象
+        # Create a socket object
         sock = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
-        # 解析域名
+        # Resolve the domain name
         try:
             sockaddr = usocket.getaddrinfo('python.quectel.com', 80)[0][-1]
         except Exception:
             print('Domain name resolution failed.')
             sock.close()
             return
-        # 建立连接
+        # Connect to the server
         sock.connect(sockaddr)
-        # 向服务端发送消息
+        # Send data to the server
         ret = sock.send('GET /News HTTP/1.1\r\nHost: python.quectel.com\r\nAccept-Encoding: deflate\r\nConnection: keep-alive\r\n\r\n')
         print('send {} bytes'.format(ret))
-        # 接收服务端消息
+        # Receive data from the server
         data = sock.recv(256)
         print('recv {} bytes:'.format(len(data)))
         print(data.decode())
-        # 关闭连接
+        # Close the connection
         sock.close()
 
-        # 用户根据需要决定是否需要在通信结束对网卡进行去激活
+        # Decide whether to deactivate the NIC after communication ends as needed
         # dataCall.deactivate(usrCfg1['profileID'])
     else:
         print('Network connected failed, stage={}, state={}'.format(stage, state))
