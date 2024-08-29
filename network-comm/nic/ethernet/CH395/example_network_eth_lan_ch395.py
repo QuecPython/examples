eac4c87cc3daaa3daec07a89aab7346499fa3097
HeyFrancisQin <127071722+HeyFrancisQin@users.noreply.github.com>
2024-08-29 11:33:02 +0800

    docs(network):对网卡demo进行细分，增加usbnetdemo (#15)
    * (network)WLAN.py有新的修改，提交最新版本至git
    
    * network: 新增slip网卡(FC41D、ESP8266、ESP8684)的三个整合包，内含对应的wifi固件、中英文readme、WLAN.py驱动文件
    
    * docs(network):对网卡demo进行细分，增加usbnetdemo
    
    把网卡demo细分为各个网卡的专用demo，并对usbnet不同的应用场景增加demo
    
    固件版本: N/A
    
    是否需要文案翻译: 否
    
    * docs(network):对网卡demo进行细分，增加usbnetdemo
    
    把网卡demo细分为各个网卡的专用demo，并对usbnet不同的应用场景增加demo
    
    固件版本: N/A
    
    是否需要文案翻译: 否

---
+++ network-comm/nic/ethernet/CH395/example_network_eth_lan_ch395.py
@@ -0,0 +1,55 @@
+# Copyright (c) Quectel Wireless Solution, Co., Ltd.All Rights Reserved.
+#  
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  
+#     http://www.apache.org/licenses/LICENSE-2.0
+#  
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import dataCall
+import ethernet
+
+nic = None
+
+def netStatusCallback(args):
+    pdp = args[0]
+    datacallState = args[1]
+    if datacallState == 0:
+        print('modem network {} disconnected.'.format(pdp))
+    elif datacallState == 1:
+        print('modem network {} connected.'.format(pdp))
+        if nic != None:
+            lte=dataCall.getInfo(1, 0)
+            print(lte)
+            nic.set_default_NIC(lte[2][2])
+
+def eth_init():
+    lte=dataCall.getInfo(1, 0)
+    print('modem net config: {}'.format(lte))
+    workMode=1
+    global nic
+    print('eth init start')
+    nic = ethernet.CH395(b'\x12\x34\x56\x78\x9a\xbc','192.168.1.1','','',-1,-1,-1,-1, workMode)
+    if lte != -1 and lte[2][0] == 1:
+        print('eth set default nic {}'.format(lte[2][2]))
+        nic.set_default_NIC(lte[2][2])
+    nic.set_dns('8.8.8.8', '114.114.114.114')
+    print('eth net config: {}'.format(nic.ipconfig()))
+    nic.set_up()
+    print('eth startup success')
+
+dataCall.setCallback(netStatusCallback)
+eth_init()
+
+
+
+
+    
+
+
